4f4797673a52aafd1f5db6cef471925e5196dd32
skez-beep <skez1998@gmail.com>
2026-04-08 10:51:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
-
 import os
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
+
+requests.get(f"https://api.telegram.org/bot{TOKEN}/deleteWebhook")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🔥 أهلاً في بوت الذهب 🔥")
